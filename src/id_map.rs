58469b9484b1267a14b83a2264caad67e47f2b72
carado <carado@carado.moe>
2020-10-02 15:07:05 +0200

    .

--- src/id_map.rs
+++ src/id_map.rs
@@ -1,26 +1,29 @@
 use {
-	super::{*, alloc_vec::AllocVec},
+	super::{*, alloc_vec::{AllocVec, FreeSetTrusted}},
 	std::{
 		hash::{Hash, BuildHasher},
 		ops::{Index, IndexMut},
 		collections::HashMap,
 		num::NonZeroUsize,
 	},
+	num_traits::NumCast,
 };
 
 type EntryHash = u32;
 
-pub struct IDMap<K, V, S = maps::std::BuildHasher> {
-	mapped: HashMap<ByKey<K, usize>, (), S>,
-	entries: AllocVec<
-		Option<std::cell::UnsafeCell<Entry<V>>>,
-		std::collections::BinaryHeap<RevOrd<usize>>,
-	>,
+pub struct IDMap<
+	K,
+	V,
+	F: FreeSetTrusted = std::collections::BinaryHeap<RevOrd<usize>>,
+	S = maps::std::BuildHasher,
+> {
+	mapped: HashMap<ByKey<K, F::Index>, (), S>,
+	entries: AllocVec<Option<std::cell::UnsafeCell<Entry<V>>>, F>,
 }
 
 struct Entry<V> { usage: NonZeroUsize, hash: u64, value: V }
 
-impl<K: Hash + Eq, V, S: Default> Default for IDMap<K, V, S> {
+impl<K, V, F: FreeSetTrusted, S: Default> Default for IDMap<K, V, F, S> {
 	fn default() -> Self {
 		Self {
 			mapped: <_>::default(),
@@ -29,7 +32,7 @@ impl<K: Hash + Eq, V, S: Default> Default for IDMap<K, V, S> {
 	}
 }
 
-impl<K: Hash + Eq, V, S: BuildHasher> IDMap<K, V, S> {
+impl<K: Hash + Eq, V, F: FreeSetTrusted, S: BuildHasher> IDMap<K, V, F, S> {
 	pub fn new() -> Self where S: Default { Self::default() }
 
 	pub fn with_hasher(hasher: S) -> Self {
@@ -41,7 +44,7 @@ impl<K: Hash + Eq, V, S: BuildHasher> IDMap<K, V, S> {
 	
 	pub fn insert<Q: maps::BorrowKey<K>>(
 		&mut self, pre_key: Q, make_value: impl FnOnce(Q) -> (K, V),
-	) -> (usize, &mut K, &mut V) {
+	) -> (F::Index, &mut K, &mut V) {
 		let entries = &mut self.entries;
 
 		let hash = self.mapped.hasher().just_hash(&pre_key);
@@ -79,7 +82,7 @@ impl<K: Hash + Eq, V, S: BuildHasher> IDMap<K, V, S> {
 	}
 
 	pub fn get<Q: maps::BorrowKey<K>>(&self, pre_key: &Q) ->
-		Option<(usize, &K, &V)>
+		Option<(F::Index, &K, &V)>
 	{
 		let hash = self.mapped.hasher().just_hash(pre_key);
 
@@ -92,37 +95,48 @@ impl<K: Hash + Eq, V, S: BuildHasher> IDMap<K, V, S> {
 			})
 	}
 
-	pub fn remove(&mut self, id: usize) -> Option<V> {
+	pub fn remove(&mut self, id: F::Index) -> Option<V> {
 		unsafe {
-			let entry = self.entries.get_mut(id)?.take()?.into_inner();
+			let opt = self.entries.get_mut(id)?;
+			let entry = &mut *opt.as_mut()?.get();
 
-			self.mapped
-				.raw_entry_mut()
-				.from_hash(entry.hash.as_(), |by| by.value == id)
-				.occupied()
-				.unsafe_unwrap();
+			match NonZeroUsize::new(entry.usage.get() - 1) {
+				Some(sub_usage) => {
+					entry.usage = sub_usage;
+					None
+				},
 
-			Some(entry.value)
+				None => {
+					self.mapped
+						.raw_entry_mut()
+						.from_hash(entry.hash.as_(), |by| by.value == id)
+						.occupied()
+						.unsafe_unwrap()
+						.remove();
+
+					Some(opt.take().unsafe_unwrap().into_inner().value)
+				},
+			}
 		}
 	}
 
-	pub unsafe fn get_unchecked(&self, id: usize) -> &V {
+	pub unsafe fn get_unchecked(&self, id: F::Index) -> &V {
 		&(&*self.entries.get_unchecked(id).unsafe_unwrap_ref().get()).value
 	}
 
-	pub unsafe fn get_unchecked_mut(&mut self, id: usize) -> &mut V {
+	pub unsafe fn get_unchecked_mut(&mut self, id: F::Index) -> &mut V {
 		&mut
 			(&mut *self.entries.get_unchecked_mut(id).unsafe_unwrap_mut().get()).value
 	}
 
-	pub fn usage(&self, id: usize) -> usize {
+	pub fn usage(&self, id: F::Index) -> usize {
 		self.entries
 			.get(id)
 			.and_then(|o| o.as_ref())
 			.map_or(0, |e| unsafe { &*e.get() }.usage.get())
 	}
 
-	pub fn key_entries(&self) -> impl Iterator<Item = (usize, &K, &V)> {
+	pub fn key_entries(&self) -> impl Iterator<Item = (F::Index, &K, &V)> {
 		self.mapped
 			.keys()
 			.map(move |by| unsafe {
@@ -132,7 +146,7 @@ impl<K: Hash + Eq, V, S: BuildHasher> IDMap<K, V, S> {
 	}
 
 	pub fn key_entries_mut(&mut self) ->
-		impl Iterator<Item = (usize, &K, &mut V)>
+		impl Iterator<Item = (F::Index, &K, &mut V)>
 	{
 		let entries = &mut self.entries;
 
@@ -145,11 +159,22 @@ impl<K: Hash + Eq, V, S: BuildHasher> IDMap<K, V, S> {
 			})
 	}
 
-	//pub fn at(&self, id: U::Raw) -> Option<(&K, &V)> {
-	//pub fn at_mut(&mut self, id: U::Raw) -> Option<(&K, &mut V)> {
+	pub fn at(&self, id: F::Index) -> Option<(&K, &V)> {
+		unsafe {
+			let entry = &*self.entries.get(id)?.as_ref()?.get();
+			Some((Self::key_fn(id, entry, &self.mapped)(), &entry.value))
+		}
+	}
+
+	pub fn at_mut(&mut self, id: F::Index) -> Option<(&K, &mut V)> {
+		unsafe {
+			let entry = &mut *self.entries.get(id)?.as_ref()?.get();
+			Some((Self::key_fn(id, entry, &self.mapped)(), &mut entry.value))
+		}
+	}
 
 	pub fn entries<'a>(&'a self) ->
-		impl Iterator<Item = (usize, &'a V, impl FnOnce() -> &'a K)>
+		impl Iterator<Item = (F::Index, &'a V, impl FnOnce() -> &'a K)>
 	{
 		let mapped = &self.mapped;
 
@@ -157,14 +182,15 @@ impl<K: Hash + Eq, V, S: BuildHasher> IDMap<K, V, S> {
 			.raw().iter()
 			.zip(0..)
 			.filter_map(move |(e, id)| unsafe {
+				let id = NumCast::from(id).unwrap();
 				let entry = &*e.as_ref()?.get();
-				let key = key_fn(id, entry, mapped);
+				let key = Self::key_fn(id, entry, mapped);
 				Some((id, &entry.value, key))
 			})
 	}
 
 	pub fn entries_mut<'a>(&'a mut self) ->
-		impl Iterator<Item = (usize, &'a mut V, impl FnOnce() -> &'a K)>
+		impl Iterator<Item = (F::Index, &'a mut V, impl FnOnce() -> &'a K)>
 	{
 		let mapped = &self.mapped;
 
@@ -172,35 +198,38 @@ impl<K: Hash + Eq, V, S: BuildHasher> IDMap<K, V, S> {
 			.raw_mut().iter_mut()
 			.zip(0..)
 			.filter_map(move |(e, id)| unsafe {
+				let id = NumCast::from(id).unwrap();
 				let entry = &mut *e.as_ref()?.get();
-				let key = key_fn(id, entry, mapped);
+				let key = Self::key_fn(id, entry, mapped);
 				Some((id, &mut entry.value, key))
 			})
 	}
+
+	unsafe fn key_fn<'a>(
+		id: F::Index,
+		entry: &Entry<V>,
+		mapped: &'a HashMap<ByKey<K, F::Index>, (), S>,
+	) -> impl FnOnce() -> &'a K {
+		let hash = entry.hash;
+
+		move || &mapped
+			.raw_entry()
+			.from_hash(hash, |by| by.value == id)
+			.unsafe_unwrap()
+			.0
+			.key
+	}
 }
 
-unsafe fn key_fn<'a, K, V, S: BuildHasher>(
-	id: usize, entry: &Entry<V>, mapped: &'a HashMap<ByKey<K, usize>, (), S>,
-) -> impl FnOnce() -> &'a K {
-	let hash = entry.hash;
-
-	move || &mapped
-		.raw_entry()
-		.from_hash(hash, |by| by.value == id)
-		.unsafe_unwrap()
-		.0
-		.key
-}
-
-impl<K, V, S> Index<usize> for IDMap<K, V, S> {
+impl<K, V, F: FreeSetTrusted, S> Index<F::Index> for IDMap<K, V, F, S> {
 	type Output = V;
-	fn index(&self, id: usize) -> &V {
+	fn index(&self, id: F::Index) -> &V {
 		&unsafe { &*self.entries[id].as_ref().unwrap().get() }.value
 	}
 }
 
-impl<K, V, S> IndexMut<usize> for IDMap<K, V, S> {
-	fn index_mut(&mut self, id: usize) -> &mut V {
+impl<K, V, F: FreeSetTrusted, S> IndexMut<F::Index> for IDMap<K, V, F, S> {
+	fn index_mut(&mut self, id: F::Index) -> &mut V {
 		&mut unsafe { &mut *self.entries[id].as_mut().unwrap().get() }.value
 	}
 }
