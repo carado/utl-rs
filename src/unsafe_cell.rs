82d480daf71b3bfb991e18be81481c894db27138
carado <carado@carado.moe>
2021-02-04 10:13:49 +0100

    .

--- src/unsafe_cell.rs
+++ src/unsafe_cell.rs
@@ -1,8 +1,16 @@
 macro_rules! base_impl {
-	($cell:ident $ref:ident $mut:ident) => {
+	(
+		$cell:ident $self:ident
+		$ref:ident { $borrow:expr }
+		$mut:ident { $borrow_mut:expr }
+		{$get_mut:expr}
+	) => {
 		impl<T> $cell<T> {
 			pub fn new(value: T) -> Self { Self(value.into()) }
 			pub fn into_inner(self) -> T { self.0.into_inner() }
+			pub unsafe fn borrow(&$self) -> $ref<T> { $ref($borrow) }
+			pub unsafe fn borrow_mut(&$self) -> $mut<T> { $mut($borrow_mut) }
+			pub fn get_mut(&mut $self) -> &mut T { $get_mut }
 		}
 
 		impl<T> From<T> for $cell<T> {
@@ -30,24 +38,14 @@ pub mod debug {
 	pub struct UnsafeCell<T: ?Sized>(parking_lot::RwLock<T>);
 
 	pub struct UnsafeCellRef<'a, T: ?Sized>(parking_lot::RwLockReadGuard<'a, T>);
-
 	pub struct UnsafeCellMut<'a, T: ?Sized>(parking_lot::RwLockWriteGuard<'a, T>);
 
-	base_impl!(UnsafeCell UnsafeCellRef UnsafeCellMut);
-
-	impl<T: ?Sized> UnsafeCell<T> {
-		pub unsafe fn get(&self) -> UnsafeCellRef<T> {
-			UnsafeCellRef(
-				self.0.try_read().expect("debug UnsafeCell caught data race")
-			)
-		}
-
-		pub unsafe fn get_mut(&self) -> UnsafeCellMut<T> {
-			UnsafeCellMut(
-				self.0.try_write().expect("debug UnsafeCell caught data race")
-			)
-		}
-	}
+	base_impl!(
+		UnsafeCell self
+		UnsafeCellRef{self.0.try_read().expect("debug UnsafeCell caught data race")}
+		UnsafeCellMut{self.0.try_write().expect("debug UnsafeCell caught data race")}
+		{self.0.get_mut()}
+	);
 }
 
 pub mod release {
@@ -58,20 +56,14 @@ pub mod release {
 	unsafe impl<T: Send> Send for UnsafeCell<T> {}
 
 	pub struct UnsafeCellRef<'a, T: ?Sized>(&'a T);
-
 	pub struct UnsafeCellMut<'a, T: ?Sized>(&'a mut T);
 
-	base_impl!(UnsafeCell UnsafeCellRef UnsafeCellMut);
-
-	impl<T: ?Sized> UnsafeCell<T> {
-		pub unsafe fn get(&self) -> UnsafeCellRef<T> {
-			UnsafeCellRef(&*self.0.get())
-		}
-
-		pub unsafe fn get_mut(&self) -> UnsafeCellMut<T> {
-			UnsafeCellMut(&mut *self.0.get())
-		}
-	}
+	base_impl!(
+		UnsafeCell self
+		UnsafeCellRef{&*self.0.get()}
+		UnsafeCellMut{&mut *self.0.get()}
+		{unsafe { &mut *self.0.get() }}
+	);
 }
 
 #[cfg(debug_assertions)]
