3e4fd948dd56183b3f33244b16b4640c582f3945
carado <carado@carado.moe>
2021-03-06 00:08:03 +0100

    .

--- src/derefs.rs
+++ src/derefs.rs
@@ -6,3 +6,28 @@ pub trait DoubleDeref<T> = Deref where
 pub trait DoubleDerefMut<T> = DerefMut where
 	<Self as Deref>::Target: DerefMut<Target = T>;
 
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Default, PartialOrd, Ord, Debug)]
+pub struct DerefInner<T: ?Sized>(pub T);
+
+impl<T> From<T> for DerefInner<T> {
+	fn from(v: T) -> Self { Self(v) }
+}
+
+impl<T> Deref for DerefInner<T> where
+	T: Deref,
+	T::Target: Deref,
+{
+	type Target = <<T as Deref>::Target as Deref>::Target;
+
+	#[inline]
+	fn deref(&self) -> &Self::Target { &**self.0 }
+}
+
+impl<T> DerefMut for DerefInner<T> where
+	T: DerefMut,
+	T::Target: DerefMut,
+{
+	#[inline]
+	fn deref_mut(&mut self) -> &mut Self::Target { &mut **self.0 }
+}
+
